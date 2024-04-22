253db8a7b926ebbcd505dd31a59bbaa2958f81e1
Mouslih1 <maromouslih@gmail.com>
2024-04-22 21:27:50 +0100

    add: mineBlock & isChainValid methode

--- src/main/java/org/example/entities/Block.java
+++ src/main/java/org/example/entities/Block.java
@@ -10,6 +10,7 @@ public class Block {
     private String previousHash;
     private String data;
     private Long timeStamp;
+    private int nonce;
 
     public Block(String data, String previousHash)
     {
@@ -23,9 +24,42 @@ public class Block {
     {
         return StringUtil.applySha256
                 (
-                  previousHash + Long.toString(timeStamp) + data
+                  previousHash + Long.toString(timeStamp) + data + Integer.toString(nonce)
                 );
     }
+
+    public void mineBlock(int difficulty)
+    {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+
+        while(!hash.substring(0, difficulty).equals(target))
+        {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public String getHash() {
         return hash;
